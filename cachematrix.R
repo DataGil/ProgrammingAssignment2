1a1669d63864f51bbd8e437c6d711540e763980f
Gilad Zafran <giladzafran@gmail.com>
2014-12-21 22:31:09 +0200

    Final with correct comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,43 @@
 makeCacheMatrix <- function(x = matrix()) {      # input x will be a vector
         
-        m <- NULL    #  m will be our 'mean' and it's reset to NULL every 
-        #    time makeVector is called
+        m <- NULL    #  m will be our 'inverse' and it's reset to NULL every 
+        #    time makeCacheMatrix is called
         
-        #  note these next three functions are defined but not run when makeVector is called.
-        #   instead, they will be used by cachemean() to get values for x or for
-        #   m (mean) and for setting the mean.  These are usually called object 'methods'
+        #  note these next three functions are defined but not run when makeCacheMatrix is called.
+        #   instead, they will be used by cacheSolve() to get values for x or for
+        #   m (inverse) and for setting the inverse.  These are usually called object 'methods'
                 
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         
-        get <- function() { x }   # this function returns the value of the original vector
+        get <- function() { x }   # this function returns the value of the original matrix
         
         setinverse <- function(solve)  { m <<- solve }
-        # this is called by cachemean() during the first cachemean()
+        # this is called by cacheSolve() during the first cacheSolve()
         #  access and it will store the value using superassignment
         
-        getinverse <- function() { m } # this will return the cached value to cachemean() on
+        getinverse <- function() { m } # this will return the cached value to cacheSolve() on
         #  subsequent accesses
         
-        list(set = set, get = get,          #  OK, this is accessed each time makeVector() is called,       
+        list(set = set, get = get,          #  OK, this is accessed each time makeCacheMatrix() is called,       
              setinverse = setinverse,  #   that is, each time we make a new object.  This is a list of 
              getinverse = getinverse)  #   the internal functions ('methods') so a calling function
         #   knows how to access those methods.                            
 }
 
 
-cacheSolve <- function(x, ...) {   # the input x is an object created by makeVector
-        m <- x$getinverse()               # accesses the object 'x' and gets the value of the mean
-        if(!is.null(m)) {              # if mean was already cached (not NULL) ...
+cacheSolve <- function(x, ...) {   # the input x is an object created by makeCacheMatrix
+        m <- x$getinverse()               # accesses the object 'x' and gets the value of the inverse
+        if(!is.null(m)) {              # if inverse was already cached (not NULL) ...
                 
                 message("getting cached data")  # ... send this message to the console
-                return(m)                       # ... and return the mean ... "return" ends 
-                #   the function cachemean(), note
+                return(m)                       # ... and return the inverse ... "return" ends 
+                #   the function cacheSolve(), note
         }
-        data <- x$get()        # we reach this code only if x$getmean() returned NULL
-        m <- solve(data, ...)   # if m was NULL then we have to calculate the mean
-        x$setinverse(m)           # store the calculated mean value in x (see setmean() in makeVector
-        m               # return the mean to the code that called this function
+        data <- x$get()        # we reach this code only if x$getinverse() returned NULL
+        m <- solve(data, ...)   # if m was NULL then we have to calculate the inverse
+        x$setinverse(m)           # store the calculated inverse value in x (see setinverse() in makeCacheMatrix
+        m               # return the inverse to the code that called this function
 }
